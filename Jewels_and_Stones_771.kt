27d0cd2a1e90a43b495e226cfec78e53cde04ddb
TengWang <markzog21@gmail.com>
2019-08-26 10:57:04 +0800

    771 done

---
+++ Jewels_and_Stones_771.kt
@@ -0,0 +1,20 @@
+import org.junit.Test
+
+class TestLeetCode {
+
+    @Test
+    fun numJewelsInStones(J: String, S: String): Int {
+        if(J.isEmpty() || S.isEmpty()) return 0
+
+        val jewel = BooleanArray(128)
+        for (c in J.toCharArray())
+            jewel[c.toInt()] = true
+
+        var res = 0
+        for (c in S.toCharArray()) {
+            if (jewel[c.toInt()]) ++res
+        }
+
+        return res
+    }
+}
