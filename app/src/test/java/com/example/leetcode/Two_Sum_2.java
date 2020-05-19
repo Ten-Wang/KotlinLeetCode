b7717f4380bc819ce954f96d31d7c3f1f2dbdd68
TengWang <markzog21@gmail.com>
2020-05-19 18:28:45 +0800

    167 done

--- app/src/test/java/com/example/leetcode/Two_Sum_2.java
+++ app/src/test/java/com/example/leetcode/Two_Sum_2.java
@@ -7,17 +7,47 @@ import java.util.Map;
 
 class Two_Sum_2 {
     @Test
-    public int[] twoSum(int[] nums, int target) {
-        int[] result = new int[2];
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-        for (int i = 0; i < nums.length; i++) {
-            if(map.containsKey(target - nums[i])){
-                result[1] = i;
-                result[0] = map.get(target - nums[i]);
-                return result;
+    public int[] twoSum(int[] numbers, int target) {
+        boolean isSmall = false;
+        long small = numbers[0];
+        long big = target - small;
+        int left = 0;
+        int right = numbers.length - 1;
+        while (true) {
+            if (isSmall) {
+                isSmall = false;
+                left = binarySearch(numbers, left, right - 1, small);
+                if (numbers[left] + numbers[right] == target) {
+                    break;
+                } else {
+                    small = numbers[++left];
+                    big = target - small;
+                }
+            } else {
+                isSmall = true;
+                right = binarySearch(numbers, left + 1, right, big);
+                if (numbers[left] + numbers[right] == target) {
+                    break;
+                } else {
+                    big = numbers[right];
+                    small = target - big;
+                }
             }
-            map.put(nums[i], i);
         }
-        return result;
+        return new int[]{left + 1, right + 1};
+    }
+
+    public int binarySearch(int[] nums, int l, int r, long target) {
+        int left = l;
+        int right = r;
+        while (left < right) {
+            int mid = right + (left - right) / 2;
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid;
+            }
+        }
+        return left;
     }
 }
