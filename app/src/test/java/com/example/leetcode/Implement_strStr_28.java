12b214bf3b9b8e31b2b21d14d3d6d692c45bb115
TengWang <markzog21@gmail.com>
2020-06-08 15:05:36 +0800

    28 done

---
+++ app/src/test/java/com/example/leetcode/Implement_strStr_28.java
@@ -0,0 +1,10 @@
+package com.example.leetcode;
+
+import org.junit.Test;
+
+public class Implement_strStr_28 {
+    @Test
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
