5019f74c22e64c8d2bc72c8d2d8ba787401d90e8
TengWang <markzog21@gmail.com>
2020-06-20 11:35:21 +0800

    190 done

---
+++ app/src/test/java/com/example/leetcode/Reverse_Bits_190.java
@@ -0,0 +1,8 @@
+package com.example.leetcode;
+
+public class Reverse_Bits_190 {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        return Integer.reverse(n);
+    }
+}
